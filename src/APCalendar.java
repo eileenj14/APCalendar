1fcd57f9f315a1c1931e61c78bac844010801c94
Eileen Jiang <eileenj14@nycstudents.net>
2025-11-10 11:48:59 -0500

    APCalendar finished.

--- src/APCalendar.java
+++ src/APCalendar.java
@@ -34,7 +34,14 @@ public class APCalendar
      */
     private static int firstDayOfYear(int year)
     {
-        return 0;
+        // Assume January 1, 0001 was a Monday.
+        int day = 1;
+        for(int i = 1; i < year; i++)
+        {
+            day++;
+            if(isLeapYear(i)) day++;
+        }
+        return day % 7;
     }
 
     /** Returns n, where month, day, and year specify the nth day of the year.
@@ -43,7 +50,23 @@ public class APCalendar
      */
     private static int dayOfYear(int month, int day, int year)
     {
-        return 0;
+        int days = 0;
+
+        if(month > 1) days += 31;
+        if(month > 2) days += 28;
+        if(isLeapYear(year)) days++;
+        if(month > 3) days += 31;
+        if(month > 4) days += 30;
+        if(month > 5) days += 31;
+        if(month > 6) days += 30;
+        if(month > 7) days += 31;
+        if(month > 8) days += 31;
+        if(month > 9) days += 30;
+        if(month > 10) days += 31;
+        if(month > 11) days += 30;
+        days += day;
+
+        return days;
     }
 
     /** Returns the value representing the day of the week for the given date
@@ -53,6 +76,7 @@ public class APCalendar
      */
     public static int dayOfWeek(int month, int day, int year)
     {
-        return 0;
+        int days = dayOfYear(month, day, year) % 7;
+        return firstDayOfYear(year) + days - 1;
     }
 }
